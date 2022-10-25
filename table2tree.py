0236a00410063d5610f64d1107932fd77bde52a0
dengzq1234 <dengziqi1234@gmail.com>
2022-10-25 14:37:33 +0200

    update annotation script

--- table2tree.py
+++ table2tree.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+
 from ete4.parser.newick import NewickError
 from ete4 import Tree, PhyloTree
+from ete4 import GTDBTaxa
+from ete4 import NCBITaxa
+import b64pickle
 
 from collections import defaultdict
 from collections import Counter
@@ -15,6 +19,7 @@ import sys
 
 TREEFILE = sys.argv[1]
 METADATAFILE = sys.argv[2]
+OUTPUTTREE = sys.argv[3]
 
 def parse_csv(input_file):
     metadata = {}
@@ -65,12 +70,15 @@ def load_metadata_to_tree(tree, metadata_dict):
             pass
     return tree
 
-def merge_annotations(nodes, target_props):
+def merge_annotations(nodes, target_props, dtype='str'):
     internal_props = {}
 
     for target_prop in target_props:
-        
-        try:
+        if dtype == 'str':
+            prop_list = children_prop_array(nodes, target_prop)
+            internal_props[add_suffix(target_prop, 'counter')] = '||'.join([add_suffix(key, value, '--') for key, value in dict(Counter(prop_list)).items()])
+            
+        elif dtype == 'num':
             prop_array = np.array(children_prop_array(nodes, target_prop),dtype=np.float64)
             n, (smin, smax), sm, sv, ss, sk = stats.describe(prop_array)
 
@@ -79,13 +87,25 @@ def merge_annotations(nodes, target_props):
             internal_props[add_suffix(target_prop, 'max')] = smax
             internal_props[add_suffix(target_prop, 'mean')] = sm
             if math.isnan(sv) == False:
-                internal_props[add_suffix(target_prop, 'variance')] = sv
+                internal_props[add_suffix(target_prop, 'std')] = sv
             else:
-                internal_props[add_suffix(target_prop, 'variance')] = 0
+                internal_props[add_suffix(target_prop, 'std')] = 0
+        # try:
+        #     prop_array = np.array(children_prop_array(nodes, target_prop),dtype=np.float64)
+        #     n, (smin, smax), sm, sv, ss, sk = stats.describe(prop_array)
 
-        except ValueError: # for strings
-            prop_list = children_prop_array(nodes, target_prop)
-            internal_props[add_suffix(target_prop, 'counter')] = '||'.join([add_suffix(key, value, '--') for key, value in dict(Counter(prop_list)).items()])
+        #     internal_props[add_suffix(target_prop, 'sum')] = np.sum(prop_array)
+        #     internal_props[add_suffix(target_prop, 'min')] = smin
+        #     internal_props[add_suffix(target_prop, 'max')] = smax
+        #     internal_props[add_suffix(target_prop, 'mean')] = sm
+        #     if math.isnan(sv) == False:
+        #         internal_props[add_suffix(target_prop, 'std')] = sv
+        #     else:
+        #         internal_props[add_suffix(target_prop, 'std')] = 0
+
+        # except ValueError: # for strings
+        #     prop_list = children_prop_array(nodes, target_prop)
+        #     internal_props[add_suffix(target_prop, 'counter')] = '||'.join([add_suffix(key, value, '--') for key, value in dict(Counter(prop_list)).items()])
 
     return internal_props
 
@@ -96,26 +116,155 @@ def children_prop_array(nodes, prop):
     array = [n.props.get(prop) for n in nodes if n.props.get(prop)] 
     return array
 
-def main():
+def annotate_taxa(tree, db="GTDB", taxid_attr="name", sp_delimiter='.', sp_field=0):
+    def return_spcode(leaf):
+        try:
+            return leaf.name.split(sp_delimiter)[sp_field]
+        except IndexError:
+            return leaf.name
+
+    if db == "GTDB":
+        gtdb = GTDBTaxa()
+        gtdb.annotate_tree(tree,  taxid_attr=taxid_attr)
+    elif db == "NCBI":
+        ncbi = NCBITaxa()
+        # extract sp codes from leaf names
+        tree.set_species_naming_function(return_spcode)
+        ncbi.annotate_tree(tree, taxid_attr="species")
+
+    # tree.annotate_gtdb_taxa(taxid_attr='name')
+    # assign internal node as sci_name
+    for n in tree.traverse():
+        if not n.is_leaf(): 
+            nleaves = str(len(n))
+            n.add_prop("nleaves", nleaves)
+        if n.name:
+            pass
+        else:
+            n.name = n.props.get("sci_name", "")
+
+    return tree
+
+def taxatree_prune(tree, rank_limit='subspecies'):
+    rank_limit = rank_limit.lower()
+    
+    ex = False
+    while not ex:
+        ex = True
+        for n in tree.iter_leaves():
+            if n.props.get('rank') != rank_limit:
+                n.detach()
+                ex = False
+
+    return tree
+
+def tree2table(tree, internal_node=True, props=[], outfile='tree2table.csv'):
+    node2leaves = {}
+    leaf2annotations = {}
+    with open(outfile, 'w', newline='') as csvfile:
+        fieldnames = props
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t', extrasaction='ignore')
+        writer.writeheader()
+        for node in tree.traverse():
+            if internal_node:
+                output_row = dict(node.props)
+                for k, prop in output_row.items():
+                    if type(prop) == list:
+                        output_row[k] = '|'.join(str(v) for v in prop)
+                writer.writerow(output_row)
+            else:
+                if node.is_leaf():
+                    output_row = dict(node.props)
+                    for k, prop in output_row.items():
+                        if type(prop) == list:
+                            output_row[k] = '|'.join(str(v) for v in prop)
+                    writer.writerow(output_row)
+                else:
+                    pass
+
+    return 
+
+def main(args=[]):
+    col_format = ['num','num','num','num','num','str']
+    pos_map = defaultdict(list)
+    for pos, ele in enumerate(col_format):
+        pos_map[ele].append(pos)
+
+    if col_format:
+        str_column = pos_map['str']
+        numerical_column = pos_map['num']
+        
+
     metadata_dict, node_props = parse_csv(METADATAFILE)
+    
+    if str_column:
+        str_props =  [node_props[i] for i in str_column]
+    if numerical_column:
+        num_props =  [node_props[i] for i in numerical_column]
+
+    str_headers = []
+    num_headers = []
+
     tree = ete4_parse(TREEFILE)
+    
+    # load annotations to leaves
     annotated_tree = load_metadata_to_tree(tree, metadata_dict)
     
+    # merge annotations
     node2leaves = annotated_tree.get_cached_content()
     for node in annotated_tree.traverse("postorder"):
         if node.is_leaf():
             pass
         else:
-            internal_props = merge_annotations(node2leaves[node], node_props)
-            node.add_prop('leaves', len(node2leaves[node]))
-            for key,value in internal_props.items():
-                node.add_prop(key, value)
+            if str_headers:
+                internal_props_text = merge_annotations(node2leaves[node], str_headers, dtype='str')
+            elif str_props:
+                internal_props_text = merge_annotations(node2leaves[node], str_props, dtype='str')
+            else:
+                internal_props_text = merge_annotations(node2leaves[node], node_props, dtype='str')
+
+            if num_headers:
+                internal_props_num = merge_annotations(node2leaves[node], num_headers, dtype='num')
+            elif num_props:
+                internal_props_num = merge_annotations(node2leaves[node], num_props, dtype='num')
             
-    # print(annotated_tree.write(properties=[]))
-    # annotated_tree
+            node.add_prop('leaves', len(node2leaves[node])) # load collapsed leaves as prop
+            
+            internal_props = {**internal_props_text, **internal_props_num}
+            for key,value in internal_props.items():
+                
+                node.add_prop(key, value)
+    
+    # taxa annotations 
+    annotated_tree = annotate_taxa(annotated_tree, taxid_attr="name")
+
+    # collapse tree by rank
+    # annotated_tree = taxatree_prune(annotated_tree, rank_limit='subspecies')
+    
     return annotated_tree
 
 
 output_tree = main()
-print(output_tree.write(properties=[]))
+
+
+# # write to pickle
+# with open(OUTPUTTREE+'.pickle', 'w') as f:
+#     f.write(b64pickle.dumps(output_tree, encoder='pickle', pack=False))
+
+# # read to pickle
+# with open(OUTPUTTREE+'.pickle', 'r') as f:
+#     file_content = f.read()
+#     print(b64pickle.loads(file_content, encoder='pickle', unpack=False))
+
+# write to newick tree
+#output_tree.write(outfile=OUTPUTTREE, properties=[], format=1)
+
+# write to tsv file
+# fieldnames = [
+#     'name', 'support', 'dist',  'leaves', 'sample1_sum', 'sample1_min', 'sample1_max', 'sample1_mean', 'sample1_variance', 'sample2_sum', 'sample2_min', 'sample2_max', 'sample2_mean', 'sample2_variance', 'sample3_sum', 'sample3_min', 'sample3_max', 'sample3_mean', 'sample3_variance', 'sample4_sum', 'sample4_min', 'sample4_max', 'sample4_mean', 'sample4_variance', 'sample5_sum', 'sample5_min', 'sample5_max', 'sample5_mean', 'sample5_variance', 'random_type_counter', 'taxid', 'sci_name', 'common_name', 'lineage', 'rank', 'named_lineage', 'nleaves', 'sample1', 'sample2', 'sample3', 'sample4','sample5','random_type' 
+# ]
+# tree2table(output_tree, internal_node=True, props=fieldnames)
+
+
+# interactive explore
 output_tree.explore(tree_name='example',layouts=[], port=5000)
