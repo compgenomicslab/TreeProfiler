664c7975b5d1fb8f190ec14d766d1f86769a822d
Jordi <jordi.burguet.castell@gmail.com>
2023-05-05 10:54:41 +0200

    treeprofiler.py: Simplify the argparser part a bit and add some comments.
    My comments appear with
      # NOTE(JBC): ...
    
    Also, whitespace cleanup. Having invisible whitespace can in rare occasions
    produce hard to find bugs. And it has no advantage.

--- treeprofiler.py
+++ treeprofiler.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from argparse import ArgumentParser
-import argparse
 import sys
+import argparse as ap
+
 import tree_annotate
 import tree_plot
 
@@ -25,37 +25,36 @@ def populate_main_args(main_args_p):
         description="Source tree input parameters")
     group.add_argument('-t', '--tree',
         type=str,
-        required=False,
         help="Input tree, .nw file, customized tree input")
     group.add_argument('--annotated_tree',
         default=False,
         action='store_true',
-        required=False,
         help="input tree already annotated by treeprofiler")
     group.add_argument('--tree_type',
         type=str,
         default='newick',
-        required=False,
         help="statistic calculation to perform for numerical data in internal nodes, [newick, ete]")
-    
+
     group.add_argument('--prop2type',
         type=str,
-        required=False,
         help="config tsv file where determine the datatype of target properties, if your input tree type is .ete, it's note necessary")
-    
+
     group = main_args_p.add_argument_group(title='Pruning parameters',
         description="Auto pruning parameters")
 
     group.add_argument('--rank_limit',
         type=str,
-        required=False,
         help="TAXONOMIC_LEVEL prune annotate tree by rank limit")
-    group.add_argument('--pruned_by', 
+    group.add_argument('--pruned_by',
         type=str,
-        required=False,
         action='append',
         help='target tree pruned by customized conditions')
-    
+
+    # NOTE(JBC): I would not use the "required=False" since it is the default
+    # and makes things more verbose.
+
+    # NOTE(JBC): I would remove all the commented code.
+
     # group = main_args_p.add_argument_group(title='OUTPUT options',
     #     description="")
     # group.add_argument('--ete4out',
@@ -71,7 +70,7 @@ def populate_main_args(main_args_p):
     #     required=False,
     #     help="output annotated tsv file")
     # group.add_argument('--out_colordict',
-    #     action="store_true", 
+    #     action="store_true",
     #     required=False,
     #     help="print color dictionary of each property")
 
@@ -79,33 +78,33 @@ def populate_main_args(main_args_p):
     #return args
 
 def main():
-    main_args_p = argparse.ArgumentParser(description=
+    main_args_p = ap.ArgumentParser(description=
         "treeprofiler.py (ver. "+__version__+
         " of "+__date__+")." + __description__+ " Authors: "+
         __author__+" ("+__email__+")",
-        formatter_class=argparse.RawTextHelpFormatter, add_help=False)
+        formatter_class=ap.RawTextHelpFormatter, add_help=False)
 
     populate_main_args(main_args_p)
 
-    parser = argparse.ArgumentParser(description="this is tree profiler ",
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = ap.ArgumentParser(description="this is tree profiler ",
+                               formatter_class=ap.RawDescriptionHelpFormatter)
     subparser = parser.add_subparsers(title="AVAILABLE PROGRAMS")
-    
-    ## - ANNOTATE - 
+
+    ## - ANNOTATE -
     annotate_args_p = subparser.add_parser('annotate', parents=[main_args_p],
                                             description='annotate tree')
     tree_annotate.populate_annotate_args(annotate_args_p)
     annotate_args_p.set_defaults(func=tree_annotate.run)
 
-    ## - PLOT - 
+    ## - PLOT -
     plot_args_p = subparser.add_parser('plot', parents=[main_args_p],
-                                            description='annotate plot')
+                                       description='annotate plot')
     tree_plot.poplulate_plot_args(plot_args_p)
-    plot_args_p.set_defaults(func=tree_plot.run )
+    plot_args_p.set_defaults(func=tree_plot.run)
 
     ## - RUN -
     args = parser.parse_args(sys.argv[1:])
     args.func(args)
-    
+
 if __name__ == '__main__':
     main()
