e918d0320a38bef8cd6a5292ecb1dd193fc7a910
dengzq1234 <dengziqi1234@gmail.com>
2024-06-05 11:19:31 +0200

    update background and colorbranch for internals

--- treeprofiler/layouts/text_layouts.py
+++ treeprofiler/layouts/text_layouts.py
@@ -279,21 +279,23 @@ class LayoutColorbranch(TreeLayout):
                                     )
 
     def set_node_style(self, node):
-        if node.is_leaf and node.props.get(self.text_prop):
-            prop_text = node.props.get(self.text_prop)
-            if prop_text:
-                if type(prop_text) == list:
-                    prop_text = ",".join(prop_text)
-                else:
-                    pass
-                if self.color_dict:
-                    node.add_face(TextFace(node.name, color = self.color_dict.get(prop_text,""), 
-                    padding_x=self.padding_x),column=0, position="branch_right")
-                    node.sm_style["hz_line_color"] = self.color_dict.get(prop_text,"")
-                    node.sm_style["hz_line_width"] = 2
-                    node.add_face(RectFace(width=self.width, height=None, color=self.absence_color, \
-                        padding_x=self.padding_x , padding_y=self.padding_y, tooltip=None),column=self.column, position="aligned")
-            
+        prop_text = node.props.get(self.text_prop)
+        if prop_text is not None:
+            if type(prop_text) == list:
+                prop_text = ",".join(prop_text)
+            else:
+                pass
+
+            if self.color_dict:
+                node.add_face(TextFace(node.name, color = self.color_dict.get(prop_text,""), 
+                padding_x=self.padding_x),column=0, position="branch_right")
+                node.sm_style["hz_line_color"] = self.color_dict.get(prop_text,"")
+                node.sm_style["hz_line_width"] = 2
+                node.sm_style["vt_line_color"] = self.color_dict.get(prop_text,"")
+                node.sm_style["vt_line_width"] = 2
+                node.add_face(RectFace(width=self.width, height=None, color=self.absence_color, \
+                    padding_x=self.padding_x , padding_y=self.padding_y, tooltip=None),column=self.column, position="aligned")
+        
         elif node.is_leaf and node.props.get(self.internal_prop):
             stackedbar_face = get_stackedbarface(node, self.internal_prop, self.color_dict, width=self.width, padding_x=self.padding_x, padding_y=self.padding_y)
             node.add_face(stackedbar_face, column = self.column, position = "aligned", collapsed_only=False)
@@ -430,37 +432,36 @@ class LayoutBackground(TreeLayout):
         super().set_tree_style(tree, tree_style)
         if self.legend:
             if self.color_dict:
+                self.color_dict['NA'] = self.absence_color
                 tree_style.add_legend(title=self.text_prop,
                                     variable='discrete',
                                     colormap=self.color_dict
                                     )
 
     def set_node_style(self, node):
-        if node.is_leaf:
-            prop_text = node.props.get(self.text_prop)
-            if prop_text:
-                if type(prop_text) == list:
-                    prop_text = ",".join(prop_text)
-                else:
-                    pass
+        prop_text = node.props.get(self.text_prop)
+        if prop_text:
+            if type(prop_text) == list:
+                prop_text = ",".join(prop_text)
+            else:
+                pass
 
-                tooltip = ""
-                if node.name:
-                    tooltip += f'<b>{node.name}</b><br>'
-                if self.text_prop:
-                    tooltip += f'<br>{self.text_prop}: {prop_text}<br>'
-                
-                if self.color_dict:
-                    color = self.color_dict.get(str(prop_text), "white")
-                    align_link_face = AlignLinkFace(width=self.width*2, height=None,
-                        stroke_color=color, stroke_width=2, line_type=0, opacity=0.8)
-                    node.sm_style["bgcolor"] = color
-                    node.sm_style["fgopacity"] = 0.5
-                    node.add_face(align_link_face,
-                        position='branch_right',
-                        #column=self.column,
-                        )
-
+            tooltip = ""
+            if node.name:
+                tooltip += f'<b>{node.name}</b><br>'
+            if self.text_prop:
+                tooltip += f'<br>{self.text_prop}: {prop_text}<br>'
+            
+            if self.color_dict:
+                color = self.color_dict.get(str(prop_text), self.absence_color)
+                align_link_face = AlignLinkFace(width=self.width*2, height=None,
+                    stroke_color=color, stroke_width=2, line_type=0, opacity=0.8)
+                node.sm_style["bgcolor"] = color
+                node.sm_style["fgopacity"] = 0.5
+                node.add_face(align_link_face,
+                    position='branch_right',
+                    #column=self.column,
+                    )
         elif node.is_leaf and node.props.get(self.internal_prop):
             stackedbar_face = get_stackedbarface(node, self.internal_prop, self.color_dict, width=self.width, padding_x=self.padding_x, padding_y=self.padding_y)
             node.add_face(stackedbar_face, column = self.column, position = "aligned", collapsed_only=False)
@@ -468,7 +469,7 @@ class LayoutBackground(TreeLayout):
         elif node.props.get(self.internal_prop):
             stackedbar_face = get_stackedbarface(node, self.internal_prop, self.color_dict, width=self.width, padding_x=self.padding_x, padding_y=self.padding_y)
             node.add_face(stackedbar_face, column = self.column, position = "aligned", collapsed_only=True)
-        else:
-            prop_face = RectFace(width=self.width, height=None, color=self.absence_color, \
-                    padding_x=self.padding_x , padding_y=self.padding_y, tooltip=None)
-            node.add_face(prop_face, column=self.column, position="aligned", collapsed_only=True)
+        # else:
+        #     prop_face = RectFace(width=self.width, height=None, color=self.absence_color, \
+        #             padding_x=self.padding_x , padding_y=self.padding_y, tooltip=None)
+        #     node.add_face(prop_face, column=self.column, position="aligned", collapsed_only=True)
